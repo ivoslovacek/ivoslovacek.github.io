63994752b3c42441a4d2beace2715a0d6929335b
Ivo Slováček <ivoslovacek@gmail.com>
2023-09-29 11:38:53 +0200

    added support for mobile

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,4 @@
+// theme switcher
 const themeToggleBtn = document.querySelector('.theme-toggle');
 const buttonDark = document.querySelector('.button-dark');
 const buttonLight = document.querySelector('.button-light');
@@ -20,3 +21,34 @@ themeToggleBtn.addEventListener('click', () => {
         buttonLight.style.display = "block";
     }
 });
+
+
+//check if the device is a mobilephone
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+const isMobile = isMobileDevice();
+console.log(isMobile)
+
+//change padding to accomodate smaller viewports
+const container = document.getElementById("container");
+
+function resizeWindow() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 700 || isMobile) {
+        container.style.padding = '0% 5%';
+    }
+    else {
+        container.style.padding = '0% 20%';
+    }
+
+    if (windowWidth < window.innerHeight) {
+        document.documentElement.style.fontSize = "36px";
+    }
+
+}
+
+resizeWindow();
+
+window.addEventListener('resize', resizeWindow);
